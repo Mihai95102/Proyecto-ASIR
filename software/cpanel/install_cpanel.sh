6b31ef6a0d429cfedc28bd6209e5e466c245e9c1
Mihai95102 <mbut182@g.educaand.es>
2023-05-16 19:51:27 +0000

    instalacion de cpanel

---
+++ software/cpanel/install_cpanel.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -x
+
+# Actualizamos los paquetes
+sudo apt-get update -y
+
+# Instalamos el comando CURL
+sudo apt-get install curl -y
+
+# Instalamos PERL
+sudo apt-get install perl
+
+# Ejecutamos el instalador de CPanel usando WGET
+cd /home && curl -o latest -L https://securedownloads.cpanel.net/latest && sh latest
