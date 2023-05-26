7419672b7b565d6e90ea39d02dc2c66ebd3d6617
Mihai95102 <mihaiconstantinbutnaru@gmail.com>
2023-05-26 06:29:54 +0000

    añadida instalacion de certbot

---
+++ infraestructura/certbot.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -x
+
+# Instalamos snap:
+sudo snap install core
+
+# Actualizamos snap:
+sudo snap refresh core
+
+# Eliminamos si existiese alguna instalación previa de certbot con apt:
+sudo apt-get remove certbot
+
+# Instalamos el cliente de Certbot con snapd:
+sudo snap install --classic certbot
+
+# Creamos una alias para el comando certbot:
+sudo ln -s /snap/bin/certbot /usr/bin/certbot
+
+# Obtenemos el certificado y configuramos el servidor web Apache:
+sudo certbot --apache -m mbut182@g.educaand.es --agree-tos --no-eff-email -d plesk.ddns.net
+
+# Comprobamos que existe un temporizador que se encarga de renovar los certificados de forma automática:
+systemctl list-timers
