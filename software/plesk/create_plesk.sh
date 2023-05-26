5a3e79ab6746e445cda92fe7bf6010dbe7d992a2
Mihai95102 <mihaiconstantinbutnaru@gmail.com>
2023-05-26 07:29:17 +0000

    cambios

---
+++ software/plesk/create_plesk.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -x
+
+# Actualizamos los paquetes
+sudo apt-get update -y
+
+# Cambiamos la interfaz de Plesk
+sudo plesk bin poweruser --off
+
+# Creamos un dominio
+sudo plesk bin domain --create proyectoasir2.ddns.net -hosting true -ip 172.31.15.51 -login mihai -passwd @W62dv8ui
--- software/plesk/install_plesk.sh
+++ software/plesk/install_plesk.sh
@@ -22,9 +22,3 @@ chmod +x ./plesk-installer
 
 # Ejecutamos el instalador de Plesk
 sudo ./plesk-installer --select-product-id plesk --select-release-latest --installation-type Recommended
-
-# Creamos un dominio
-sudo plesk bin domain --create proyectoasir2.ddns.net -hosting true -ip 34.230.204.213 -login mihai -passwd @W62dv8ui
-
-# Añadimos la extensión de AcronisBackup
-sudo plesk bin extension --install AcronisBackup
