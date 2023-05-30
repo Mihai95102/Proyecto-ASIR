3dce68bc6449d5fc8c4a116c06f5e5f13a5bc73e
Mihai95102 <mbut182@g.educaand.es>
2023-05-30 20:06:06 +0000

    cambios

--- software/plesk/create_plesk.sh
+++ software/plesk/create_plesk.sh
@@ -9,4 +9,4 @@ sudo apt-get update -y
 sudo plesk bin poweruser --off
 
 # Creamos un dominio
-sudo plesk bin domain --create proyectoasir2.ddns.net -hosting true -ip 172.31.15.51 -login mihai -passwd @W62dv8ui
+sudo plesk bin domain --create proyectoasir2.ddns.net -hosting true -ip 172.31.83.139 -login mihai -passwd @W62dv8ui
