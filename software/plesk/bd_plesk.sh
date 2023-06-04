89956fbcd917eabe249f61b4c5b0720bfb3e2eb8
Mihai95102 <mbut82@g.educaand.es>
2023-06-04 18:26:47 +0000

    cambios

--- software/plesk/bd_plesk.sh
+++ software/plesk/bd_plesk.sh
@@ -9,23 +9,23 @@ source ../vars/variables.sh
 apt-get update
 
 # Instalamos el servidor de Bases de Datos
-apt-get install mysql-server -y
+apt-get install mariadb-server -y
 
 # Cambiamos el archivo de configuración de MySQL
-cp conf/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
+cp conf/50-server.cnf /etc/mysql/mariadb.conf.d/50-server.cnf
 
 # Creamos bases de datos, usuarios y permisos
-mysql -u root -e "CREATE DATABASE host1_psa;"
+# mysql -u root -e "CREATE DATABASE host1_psa;"
 mysql -u root -e "CREATE USER '$USUARIOPLESK'@'%' IDENTIFIED BY 'hola';"
-mysql -u root -e "GRANT ALL ON host1_psa.* TO '$USUARIOPLESK'@'%' WITH GRANT OPTION;"
+mysql -u root -e "GRANT ALL ON *.* TO '$USUARIOPLESK'@'%' WITH GRANT OPTION;"
 
-mysql -u root -e "CREATE DATABASE host1_apsc;"
-mysql -u root -e "CREATE USER '$USUARIOAPSC'@'%' IDENTIFIED BY '$PASSWD';"
-mysql -u root -e "GRANT ALL ON host1_apsc.* TO '$USUARIOAPSC'@'%' WITH GRANT OPTION;"
+# mysql -u root -e "CREATE DATABASE host1_apsc;"
+# mysql -u root -e "CREATE USER '$USUARIOAPSC'@'%' IDENTIFIED BY '$PASSWD';"
+# mysql -u root -e "GRANT ALL ON host1_apsc.* TO '$USUARIOAPSC'@'%' WITH GRANT OPTION;"
 
-mysql -u root -e "CREATE DATABASE host1_roundcubemail;"
-mysql -u root -e "CREATE USER '$USUARIOMAIL'@'%' IDENTIFIED BY '$PASSWD';"
-mysql -u root -e "GRANT ALL ON host1_roundcubemail.* TO '$USUARIOMAIL'@'%' WITH GRANT OPTION;"
+# mysql -u root -e "CREATE DATABASE host1_roundcubemail;"
+# mysql -u root -e "CREATE USER '$USUARIOMAIL'@'%' IDENTIFIED BY '$PASSWD';"
+# mysql -u root -e "GRANT ALL ON host1_roundcubemail.* TO '$USUARIOMAIL'@'%' WITH GRANT OPTION;"
 
 # Reiniciamos el servidor de MySQL
 systemctl restart mysql
