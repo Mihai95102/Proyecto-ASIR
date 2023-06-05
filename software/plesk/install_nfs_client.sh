58f59de46c7db627c5c297fdcc0530da9186661d
Mihai95102 <mbut182@g.educaand.es>
2023-06-05 10:42:59 +0000

    cambios

--- software/plesk/install_nfs_client.sh
+++ software/plesk/install_nfs_client.sh
@@ -6,5 +6,12 @@ set -x
 source ../vars/variables.sh
 
 # Instalamos el cliente NFS
-apt-get install nfs-common
+apt-get install nfs-common -y
 
+# Movemos los vhosts existentes a una nueva carpeta
+mv /var/www/vhosts /var/www/vhosts.original
+
+# Creamos una nueva carpeta para vhosts
+mkdir /var/www/vhosts
+
+# Añadimos contenido al archivo /etc/fstab
