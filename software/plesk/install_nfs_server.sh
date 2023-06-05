5f16a309f539a417b9431c5f0d066aa67fbc13c5
Mihai95102 <113906544+Mihai95102@users.noreply.github.com>
2023-06-05 13:51:56 +0200

    Update install_nfs_server.sh

--- software/plesk/install_nfs_server.sh
+++ software/plesk/install_nfs_server.sh
@@ -11,8 +11,13 @@ apt-get update
 # Instalamos el NFS Server
 apt-get install nfs-kernel-server -y
 
+# Eliminamos los directorios por si existen
+rm -rf /var/nfs/plesk-1/vhosts
+rm -rf /var/nfs/plesk-2/vhosts
+
 # Creamos un directorio donde Plesk almacenará los archivos de los sitios web
 mkdir -p /var/nfs/plesk-1/vhosts
+mkdir -p /var/nfs/plesk-2/vhosts
 
 # Actualizamos el archivo /etc/exports
 cp conf/exports /etc/exports
